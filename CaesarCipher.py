7304d0783313efeda851a0340d4a8f5f0d4ed488
jin-r07 <rai.john0707@gmail.com>
2024-08-12 11:54:00 +0545

    feat: changes in Caesar cipher

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -1,14 +1,15 @@
 def CaesarCipher():
     while True:
-        option = input("Enter the option: \n1. Encrypt \n2. Decrypt \n3. Exit.\n")
+        option = input("------- Caesar Cipher ------\nEnter the option: \n1. Encrypt \n2. Decrypt \n3. Exit.\n")
         encoded_text = ""
         decoded_text = ""
 
         if option == "1":
             plain_text = input("Plain text: ")
+            shift = int(input("Enter the shift number: "))
             for char in plain_text:
                 if char.isalpha():
-                    shifted = ord(char) + 3
+                    shifted = ord(char) + shift
                     if char.islower():
                         if shifted > ord('z'):
                             shifted -= 26
@@ -23,9 +24,10 @@ def CaesarCipher():
 
         elif option == "2":
             encoded_text = input("Encrypted text: ")
+            shift = int(input("Enter the shift number: "))
             for char in encoded_text:
                 if char.isalpha():
-                    shifted = ord(char) - 3
+                    shifted = ord(char) - shift
                     if char.islower():
                         if shifted < ord('a'):
                             shifted += 26
@@ -43,6 +45,5 @@ def CaesarCipher():
 
         else:
             print("Invalid option. Please choose 1, 2, or 3.")
-
 
 CaesarCipher()
