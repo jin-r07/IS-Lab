4e531a46e729804f540263566f031955131f4ac0
jin-r07 <rai.john0707@gmail.com>
2024-08-13 13:11:42 +0545

    feat: Euclidean Algorithm

--- Euclidean Algorithm.py
+++ Euclidean Algorithm.py
@@ -1,20 +1,24 @@
-def euclidean(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
+import math
 
-def main():
+
+def gcd(a, b):
+    return math.gcd(a, b)
+
+
+def EuclideanAlgorithm():
     while True:
-        option = input("------- Euclidean Algorithm ------\nEnter the option: \n1. Find GCD \n2. Exit\n")
+        option = input("------- Euclidean Algorithm ------\nEnter the option: \n1. Find GCD \n2. Exit.\n")
 
         if option == "1":
             try:
                 a = int(input("Enter the first number: "))
                 b = int(input("Enter the second number: "))
-                gcd = euclidean(a, b)
-                print(f"The GCD of {a} and {b} is {gcd}\n")
             except ValueError:
                 print("Invalid input. Please enter integers.\n")
+                continue
+
+            result = gcd(a, b)
+            print(f"The GCD of {a} and {b} is {result}\n")
 
         elif option == "2":
             print("Exiting...")
@@ -23,5 +27,5 @@ def main():
         else:
             print("Invalid option. Please choose 1 or 2.\n")
 
-if __name__ == "__main__":
-    main()
+
+EuclideanAlgorithm()
